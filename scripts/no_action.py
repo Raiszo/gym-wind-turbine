40028fb3ce50c311c389a46080d6b3dfae673667
Raiszo <E05.sek@gmail.com>
2021-11-01 22:50:24 -0500

    feat: script to simulate no action on the turbine

--- scripts/no_action.py
+++ scripts/no_action.py
@@ -1,15 +1,30 @@
 import gym
-import gym_wind_turbine
 import numpy as np
+import gym_wind_turbine
+from gym_wind_turbine.envs.wind_turbine_analytical import WindTurbineAnalytical
+from rl_agents.ppo import get_env
+from scripts.plots import make_plots
 
-env = gym.make('WindTurbine-analytical-v0', record=True)
 
-obs = env.reset()
+def run_no_action(env: gym.Wrapper, max_steps: int) -> float:
+    reward_sum = 0.0
+    state = env.reset()
 
-while True:
-# for i in range(20):
-    obs, reward, done, info = env.step(np.array([0.0]))
-    print(obs, reward, done)
+    for i in range(1, max_steps + 1):
+        state, reward, done, _ = env.step(np.array([0.0]))
 
-    if done:
-        break
+        if done:
+            break
+
+    assert isinstance(env.env, WindTurbineAnalytical)
+
+    rec, dt = env.env.get_recordings()
+    make_plots(rec, dt)
+
+
+    return reward_sum
+
+if __name__ == '__main__':
+    # env = get_env('WindTurbine-analytical-v0', record=True, omega_0=0.9, T_gen_0=500.0)
+    env = get_env('WindTurbine-analytical-v0', record=True)
+    run_no_action(env, 240*20)

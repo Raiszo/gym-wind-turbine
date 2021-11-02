1d016999cd311723a160e4250d4caf38f0b7cd43
Raiszo <E05.sek@gmail.com>
2021-11-01 22:53:45 -0500

    feat: 180 seconds of simulation seems fine for constant wind

--- gym_wind_turbine/__init__.py
+++ gym_wind_turbine/__init__.py
@@ -3,5 +3,5 @@ from gym.envs.registration import register
 register(
     id='WindTurbine-analytical-v0',
     entry_point='gym_wind_turbine.envs:WindTurbineAnalytical',
-    max_episode_steps=int(60.0/0.05), # 30s -> 600 steps
+    max_episode_steps=int(180.0/0.05),
 )
